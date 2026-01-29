890c493984f67da5884b97fc872a1e19717bb596
Thiago Lima <tlima7601@gmail.com>
2026-01-29 12:19:41 -0300

    Produtos inseridos na tela

--- mercado-livre/src/components/main/Main.tsx
+++ mercado-livre/src/components/main/Main.tsx
@@ -1,3 +1,4 @@
+import { products } from "../../db/data";
 import './Main.css';
 
 const Main = () => {
@@ -6,6 +7,19 @@ const Main = () => {
             <div className="container">
                 <h2 className="section-title">Ofertas Do dia</h2>
                 <div className="products-grid" id="products-grid">
+                    {products && products.map((p) => (
+                        <div key={p.id} className="product-card">
+                            <img className="product-image" src={p.image} alt={p.title} />
+                            <h3 className="product-title">{p.title}</h3>
+                            <div className="product-price">
+                                <span className="price">{p.price}</span>
+                            </div>
+                            <div className="product-discount">
+                                <span className="discount">{p.discount}</span>
+                            </div>
+                        </div>
+                    ))
+                    }
                 </div>
             </div>
         </main>
